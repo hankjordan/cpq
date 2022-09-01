939177fd08fef73220323c0bb0577bc61102fccb
hank <hank@hank.co.in>
2022-08-31 21:12:31 -0400

    custom data structure

--- examples/main.rs
+++ examples/main.rs
@@ -7,7 +7,19 @@ pub struct Location {
     pub z: i32,
 }
 
+impl Location {
+    pub fn new(x: i32, y: i32, z: i32) -> Self {
+        Self { x, y, z }
+    }
+}
+
 fn main() {
     let cpq: ConcurrentPriorityQueue<Location, u32> = ConcurrentPriorityQueue::new();
-    println!("Peek: {:?}", cpq.peek());
+    
+    cpq.push(Location::new(0, 0, 0), 0);
+    cpq.push(Location::new(0, 0, 0), 2);
+    cpq.push(Location::new(5, 0, 0), 7);
+    cpq.push(Location::new(0, 6, 0), 2);
+
+    println!("POP {:?}", cpq.pop());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,69 @@
 use std::collections::hash_map::{RandomState};
+use std::mem::ManuallyDrop;
+use std::ptr;
+use std::sync::atomic::Ordering::{Acquire, Release, Relaxed};
 
 use std::hash::{Hash, BuildHasher};
 
-pub mod iterators;
-pub mod refs;
+use crossbeam::epoch::{self, Atomic, Owned};
 
-use crossbeam_epoch as epoch;
+struct Node<I, P, H = RandomState> {
+    item: ManuallyDrop<I>,
+    priority: ManuallyDrop<P>,
+    hasher: ManuallyDrop<H>,
 
-use crossbeam_skiplist_piedb::SkipList;
-use dashmap::DashMap;
-pub use iterators::*;
-pub use refs::*;
+    next: Atomic<Node<I, P, H>>,
+    next_item: Atomic<Node<I, P, H>>,
+    next_priority: Atomic<Node<I, P, H>>,
+}
+
+impl<I, P, H> Node<I, P, H> {
+    fn new(item: I, priority: P, hasher: H) -> Self {
+        Self {
+            item: ManuallyDrop::new(item),
+            priority: ManuallyDrop::new(priority),
+            hasher: ManuallyDrop::new(hasher),
+            next: Atomic::null(),
+            next_item: Atomic::null(),
+            next_priority: Atomic::null(),
+        }
+    }
+}
 
 pub struct ConcurrentPriorityQueue<I, P, H = RandomState>
 where
     I: Hash + Eq,
     P: Ord,
+    H: BuildHasher + Clone,
 {
-    items: DashMap<I, P, H>,
-    priorities: SkipList<P, Vec<I>>,
+    hasher: H,
+
+    head: Atomic<Node<I, P, H>>,
+    items_head: Atomic<Node<I, P, H>>,
+    priorities_head: Atomic<Node<I, P, H>>,
 }
+
+impl<I, P, H> Drop for ConcurrentPriorityQueue<I, P, H>
+where
+    I: Hash + Eq,
+    P: Ord,
+    H: BuildHasher + Clone,
+{
+    fn drop(&mut self) {
+        while self.pop().is_some() {}
+    }
+}
+
+impl<I, P> Default for ConcurrentPriorityQueue<I, P>
+where
+    I: Hash + Eq,
+    P: Ord,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 
 impl<I, P> ConcurrentPriorityQueue<I, P> 
 where
@@ -27,11 +71,7 @@ where
     P: Ord,
 {
     pub fn new() -> Self {
-        Self::with_capacity(0)
-    }
-
-    pub fn with_capacity(capacity: usize) -> Self {
-        Self::with_capacity_and_default_hasher(capacity)
+        Self::with_default_hasher()
     }
 }
 
@@ -42,11 +82,7 @@ where
     H: BuildHasher + Clone + Default,
 {
     pub fn with_default_hasher() -> Self {
-        Self::with_capacity_and_default_hasher(0)
-    }
-
-    pub fn with_capacity_and_default_hasher(capacity: usize) -> Self {
-        Self::with_capacity_and_hasher(capacity, H::default())
+        Self::with_hasher(H::default())
     }
 }
 
@@ -57,61 +93,70 @@ where
     H: BuildHasher + Clone
 {
     pub fn with_hasher(hash_builder: H) -> Self {
-        Self::with_capacity_and_hasher(0, hash_builder)
-    }
-
-    pub fn with_capacity_and_hasher(capacity: usize, hash_builder: H) -> Self {
         Self {
-            items: DashMap::with_capacity_and_hasher(capacity, hash_builder),
-            priorities: SkipList::new(epoch::default_collector().clone()),
+            hasher: hash_builder,
+            head: Atomic::null(),
+            items_head: Atomic::null(),
+            priorities_head: Atomic::null(),
         }
-    }
-
-    /// Returns an iterator in arbitrary order over the
-    /// (item, priority) elements in the queue
-    pub fn iter(&self) -> dashmap::iter::Iter<'_, I, P, H> {
-        self.items.iter()
     }
 }
 
-impl<'a, I, P, H> ConcurrentPriorityQueue<I, P, H>
+impl<I, P, H> ConcurrentPriorityQueue<I, P, H>
 where
     I: Hash + Eq,
     P: Ord,
     H: BuildHasher + Clone
 {
-    pub fn iter_mut(&mut self) -> dashmap::iter::IterMut<'_, I, P, H> {
-        self.items.iter_mut()
-    }
+    pub fn push(&self, item: I, priority: P) {
+        let mut n = Owned::new(Node::new(
+            item,
+            priority,
+            self.hasher.clone(),
+        ));
 
-    pub fn peek(&'a self) -> Option<Ref<'a, I, P, H>>  {
         let guard = epoch::pin();
 
-        if let Some(entry) = self.priorities.front(&guard) {
-            if let Some(key) = entry.value().get(0) {
-                let idx = self.items.determine_map(key);
+        loop {
+            let head = self.head.load(Relaxed, &guard);
 
-                let shard = unsafe { self.items.shards().get_unchecked(idx).read() };
+            n.next.store(head, Relaxed);
 
-                let (item, priority) = (key, entry.key());
-                
-                unsafe {
-                    let i_ptr: *const I = item;
-                    let p_ptr: *const P = priority;
-
-                    return Some(Ref::new(guard, shard, i_ptr, p_ptr));
-                }
+            match self.head.compare_exchange(head, n, Release, Relaxed, &guard) {
+                Ok(_) => break,
+                Err(e) => n = e.new,
             }
         }
-
-        None
-    }
-
-    pub fn capacity(&self) -> usize {
-        todo!()
     }
 
     pub fn pop(&self) -> Option<(I, P)> {
+        let guard = epoch::pin();
+        loop {
+            let head_shared = self.head.load(Acquire, &guard);
+            match unsafe { head_shared.as_ref() } {
+                Some(head) => {
+                    let next = head.next.load(Relaxed, &guard);
+                    if self
+                        .head
+                        .compare_exchange(head_shared, next, Release, Relaxed, &guard)
+                        .is_ok()
+                    {
+                        unsafe {
+                            guard.defer_destroy(head_shared);
+
+                            let item = ManuallyDrop::into_inner(ptr::read(&(*head).item));
+                            let priority = ManuallyDrop::into_inner(ptr::read(&(*head).priority));
+
+                            return Some((item, priority));
+                        }
+                    }
+                }
+                None => return None,
+            }
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
         todo!()
     }
 
@@ -120,6 +165,7 @@ where
     }
 
     pub fn is_empty(&self) -> bool {
-        todo!()
+        let guard = epoch::pin();
+        self.head.load(Acquire, &guard).is_null()
     }
 }
